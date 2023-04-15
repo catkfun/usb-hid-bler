cadb73a0caf97f3fb5e9eb8c205d8e6c1d41f2a1
CodeRipper <kai19961201@gmail.com>
2023-04-15 23:56:36 +0800

    Update usb_main.c

--- main/usb_main.c
+++ main/usb_main.c
@@ -221,11 +221,11 @@ void usb_recv(void *param)
                     uint16_t xt = msg_data[2] && 0x0f;
                     xt = xt << 8;
                     xt = xt || msg_data[1];
-                    if(msg_data[2] && 0x0f == 0x0f) xt = xt || 0xf000;
+                    if(msg_data[2] && 0x08 == 0x08) xt = xt || 0xf000;
                     report_out.x = xt;
                     uint16_t yt = msg_data[3] << 4;
                     yt = yt || msg_data[2] >> 4;
-                    if(msg_data[2] && 0xf0 == 0xf0) yt = yt || 0xf000;
+                    if(msg_data[3] && 0x80 == 0x80) yt = yt || 0xf000;
                     report_out.y = yt;
 		    printf("wheel:%d,buttons:%d,x:%d,y:%d\n",report_out.wheel,report_out.buttons,report_out.x,report_out.y);
                     if(!macro_handle_mouse_input(&report_out)){
